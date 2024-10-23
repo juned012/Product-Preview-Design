f0aa1e01ae33b57c66e904036f18e6b0a39b7fbf
juned072 <junedansari9172@gmail.com>
2024-10-23 17:35:49 +0530

    Added left and right arrow functionality and auto sliding feature

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+const mainImage = document.getElementById("mainImage");
+
 function changeImage(imageIndex) {
-  const mainImage = document.getElementById("mainImage");
   mainImage.src = imageIndex;
 }
+
+const imgArray = [
+  "/image/img1.webp",
+  "/image/img2.jpeg",
+  "/image/img3.webp",
+  "/image/img4.webp",
+];
+let index = 0;
+
+function nextPre() {
+  if (index < imgArray.length - 1) {
+    index++;
+  } else {
+    index = 0;
+  }
+  mainImage.src = imgArray[index];
+}
+
+function previousPre() {
+  if (index > 0) {
+    index--;
+  } else {
+    index = imgArray.length - 1;
+  }
+  mainImage.src = imgArray[index];
+}
+
+setInterval(nextPre, 4000);
